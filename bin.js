e5efdb1c46f04ab16857c310f2d1000ff6881df9
Raine Revere <raine@cybersemics.org>
2024-03-17 13:28:20 +0000

    Add hashbang to bin.

--- bin.js
+++ bin.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 const path = require('path')
 const sieve = require('./index')
 const filename = process.argv[2]
 const filters = require(path.resolve(filename))
 const output = sieve(filters)
-console.log(output)
+console.info(output)
